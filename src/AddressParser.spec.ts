ed978bb4938bf2766c522e00079c5ce4afc2d5b3
Felix Nehrke <felix@nehrke.info>
2026-01-11 23:27:52 +0100

    Extract streets and housenumbers for test into dedicated variables

--- src/AddressParser.spec.ts
+++ src/AddressParser.spec.ts
@@ -5,14 +5,7 @@ describe('AddressParser', () => {
   const addressParser = new AddressParser()
 
   describe('splitStreetAndHousenumber', () => {
-    it('should provide a tuple of street and housenumber', () => {
-      const tuple = addressParser.splitStreetAndHousenumber('')
-      expect(tuple).toHaveLength(2)
-      expect(tuple[0]).toBeTypeOf('string')
-      expect(tuple[1]).toBeTypeOf('string')
-    })
-
-    describe.each([
+    const streets = [
       'Am Kaiserkai',
       'Güntherstraße',
       'Clemens-Schultz-Str.',
@@ -20,26 +13,36 @@ describe('AddressParser', () => {
       'Brücke des 17. Juni',
       '1. Hafenstraße',
       'D4', // Special case "Quadratestadt"
-    ])('street: "%s"', street => {
-        describe.each([
-          '',
-          '7',
-          '13',
-          '7 a',
-          '7 B',
-          '7 - 13',
-          '7 / 8',
-          '7 A - 13 b',
-          '7 a / 8 b',
-          '17 1/2', // special case in cologne
-          '7 // App. 3' // special case of apartments and similar
-        ])('housenumber: "%s"', housenumber => {
-            it('should split street and housenumber into a tuple', () => {
-              const streetAndHousenumber = `${street} ${housenumber}`
-              expect(addressParser.splitStreetAndHousenumber(streetAndHousenumber))
-                .toEqual([street, housenumber])
-            })
-          })
+    ]
+    const housenumbers = [
+      '',
+      '7',
+      '13',
+      '7 a',
+      '7 B',
+      '7 - 13',
+      '7 / 8',
+      '7 A - 13 b',
+      '7 a / 8 b',
+      '17 1/2', // special case in cologne
+      '7 // App. 3' // special case of apartments and similar
+    ]
+
+    it('should provide a tuple of street and housenumber', () => {
+      const tuple = addressParser.splitStreetAndHousenumber('')
+      expect(tuple).toHaveLength(2)
+      expect(tuple[0]).toBeTypeOf('string')
+      expect(tuple[1]).toBeTypeOf('string')
+    })
+
+    describe.each(streets)('street: "%s"', street => {
+      describe.each(housenumbers)('housenumber: "%s"', housenumber => {
+        it('should split street and housenumber into a tuple', () => {
+          const streetAndHousenumber = `${street} ${housenumber}`
+          expect(addressParser.splitStreetAndHousenumber(streetAndHousenumber))
+            .toEqual([street, housenumber])
+        })
       })
+    })
   })
 })
