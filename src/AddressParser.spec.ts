b1da5f67a4edd7c8fa5c484ed621bec47e807a06
Felix Nehrke <felix@nehrke.info>
2026-01-11 23:22:15 +0100

    Cosider apartment-numbers and similar in housenumbers during parsing

--- src/AddressParser.spec.ts
+++ src/AddressParser.spec.ts
@@ -30,7 +30,8 @@ describe('AddressParser', () => {
           '7 / 8',
           '7 A - 13 b',
           '7 a / 8 b',
-          '17 1/2', // <-- special case in cologne
+          '17 1/2', // special case in cologne
+          '7 // App. 3' // special case of apartments and similar
         ])('housenumber: "%s"', housenumber => {
             it('should split street and housenumber into a tuple', () => {
               const streetAndHousenumber = `${street} ${housenumber}`
